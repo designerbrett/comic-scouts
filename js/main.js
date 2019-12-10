71caac1eb45df350dd1175dca08ee2f27e9ff232
Brett Anderson <brett@kwallcompany.com>
2019-12-10 10:49:20 -0800

    Major Updates to styling

--- js/main.js
+++ js/main.js
@@ -200,3 +200,13 @@ var buttonFilter = {
 		}
   	}
 };
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
