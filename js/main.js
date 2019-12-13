feb8a15ae812b4b3bcffa5fb6c2a232804e50e1a
Brett Anderson <brett@kwallcompany.com>
2019-12-13 15:37:32 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -255,7 +255,7 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
-var ul = document.querySelector('ul#hr');
+var ul = document.querySelector('ul');
 for (var i = ul.children.length; i >= 0; i--) {
     ul.appendChild(ul.children[Math.random() * i | 0]);
 }
