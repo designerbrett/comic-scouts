b36f9dfb8caaebe1bae91f8587ad0160fee8b49f
Brett Anderson <brett@kwallcompany.com>
2019-12-13 22:12:37 -0800

    all filtering

--- js/main.js
+++ js/main.js
@@ -255,7 +255,21 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
-var ul = document.querySelector('ul');
-for (var i = ul.children.length; i >= 0; i--) {
-    ul.appendChild(ul.children[Math.random() * i | 0]);
+
+// All Search Filtering
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("hr");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
